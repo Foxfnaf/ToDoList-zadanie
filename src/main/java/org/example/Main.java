dd7cede2d9c5b8be5148530711eb8d3a9c676e4a
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-07-31 08:21:33 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Tasks tasks = new Tasks();
 
+        System.out.println("1: Dodaj zadanie");
+        System.out.println("2: Wywietś");
     }
 }
