f778d7cd624c6183b010c486ec5d1309c46cefa1
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-07-31 10:54:13 +0200

    ToDo List (jeszcze w pełni nie działa)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Tasks tasks = new Tasks();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1: Dodaj zadanie");
-        System.out.println("2: Wywietś");
+        int userChoice;
+
+        while (true) {
+            System.out.println("1: Dodaj zadanie");
+            System.out.println("2: Wyświetl zadania");
+            System.out.println("3: Usuń zadanie");
+            System.out.println("4: Modifikuj zadanie");
+
+            userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1 -> tasks.addTask();
+                case 2 -> System.out.println(tasks.printTask());
+                case 3 -> tasks.removeTask();
+                case 4 -> tasks.modifyTask();
+            }
+        }
     }
 }
--- src/main/java/org/example/Tasks.java
+++ src/main/java/org/example/Tasks.java
@@ -6,15 +6,34 @@ import java.util.Scanner;
 
 public class Tasks {
     private List<String> tasks = new ArrayList<>();
+    public int i = 0;
     Scanner scanner = new Scanner(System.in);
+    int taskIndeks;
 
-    private void addTask() {
-        System.out.println("Wpisz swoje zadanie");
+
+    public void addTask() {
+        System.out.println("Wpisz swoje zadanie.");
         String task = scanner.nextLine();
-        tasks.add(task);
+        tasks.add(i + " " + task);
     }
 
-    private List<String> getTask() {
+    public List<String> printTask() {
         return tasks;
+    }
+
+    public void removeTask() {
+
+        System.out.println("Podaj numer zadania (Możesz je sprawdzić wyświetlająć wszystkie zadania. Numery zaczynają się od 0.)");
+       taskIndeks = scanner.nextInt();
+        tasks.remove(taskIndeks);
+    }
+
+    public void modifyTask() {
+        System.out.println("Podaj numer zadania (Numery zaczynają się od 0)");
+        taskIndeks = scanner.nextInt();
+
+        System.out.println("Podaj nowy tekst zadania");
+        String userText = scanner.nextLine();
+        tasks.set(taskIndeks, userText);
     }
 }
